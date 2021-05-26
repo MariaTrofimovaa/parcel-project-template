baf8ff973582716c4cf87f691279581b3166e7ca
Valerian Rykov <Valerra2@gmail.com>
2021-05-26 14:04:40 +0300

    push

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,4 +1,12 @@
 import calendarTpl from '../templates/calendar.hbs';
-import ApiService from '../js/apiService.js';
+import apiService from '../js/apiService.js';
 
-const calendar = (document.querySelector('.date-quote').innerHTML = calendarTpl());
+apiService.getData('weather').then(data => {
+  document.querySelector('.date').insertAdjacentHTML('beforeend', calendarTpl(data));
+  console.log(data);
+});
+
+apiService.getData('forecast').then(data1 => {
+  document.querySelector('.date').insertAdjacentHTML('afterend', calendarTpl(data1));
+  console.log(data1);
+});
