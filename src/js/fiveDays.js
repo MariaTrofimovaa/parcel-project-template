5d3d57e992625d762d9b7441988bb1b8569f5fbc
Russel <blokhin.rus@gmail.com>
2021-05-27 10:35:30 +0300

    inProgres

--- src/js/fiveDays.js
+++ src/js/fiveDays.js
@@ -1,4 +1,21 @@
 import fiveDaysTpl from '../templates/fiveDays.hbs';
-import ApiService from '../js/apiService.js';
+import arrowLeft from '../images/arrowLeft.png';
+import arrowRight from '../images/arrowRight.png';
+// import oneDayTpl from '../templates/fiveDays.hbs';
+import apiService from '../js/apiService.js';
 
-document.querySelector('.fivedays-weather').innerHTML = fiveDaysTpl();
+// apiService.getData('forecast').then(data => {
+//   document.querySelector('.fivedays-weather').innerHTML = fiveDaysTpl(data);
+//   console.log(data);
+// });
+
+apiService.getData('forecast').then(data => {
+  const tplDate = {
+    arrowLeft,
+    arrowRight,
+    ...data,
+    icon: `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`,
+  };
+  document.querySelector('.fivedays-weather').innerHTML = fiveDaysTpl(tplDate);
+  console.log(data);
+});
--- src/js/oneDay.js
+++ src/js/oneDay.js
@@ -1,4 +1,20 @@
 import oneDayTpl from '../templates/oneDay.hbs';
-import ApiService from '../js/apiService.js';
+import apiService from '../js/apiService.js';
 
-document.querySelector('.one-day').innerHTML = oneDayTpl();
+// document.querySelector('.one-day').innerHTML = oneDayTpl();
+
+apiService.getData('weather').then(data => {
+  // data = data.map(e => Math.floor(e.main.temp - 273.15));
+  // const temp = {
+  //   TempMin: Math.min(...data),
+  //   TempMax: Math.max(...data),
+  // };
+
+  const tplDate = {
+    ...data,
+    // temp: Math.round(...data),
+    icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+  };
+  document.querySelector('.one-day').innerHTML = oneDayTpl(tplDate);
+  console.log(data);
+});
