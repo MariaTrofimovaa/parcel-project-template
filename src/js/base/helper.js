b1353f99ff403508d1b1388056123374f21f17de
Andrii Kyreikov <Andropolog@gmail.com>
2021-05-28 10:14:12 +0300

    more-info

--- src/js/base/helper.js
+++ src/js/base/helper.js
@@ -1,176 +1,80 @@
 import apiService from '../apiService.js';
+import oneDay from '../oneDay.js';
+import fiveDays from '../fiveDays.js';
 
-let location = '';
-let req = '';
-let oneDayData = {};
-let fiveDayData = {};
-let moreInfoData = {};
+// const today = document.querySelector('.one-day');
+// const cityNameCurr = document.querySelector('.city-title');
 
-const input = document.querySelector('.search-box');
-const main = document.querySelector('.city-title');
+// Делаем запрос на сервер и получаем данные за один день
+const getOneDayData = () => {
+  return apiService
+    .getData('weather')
+    .then(data => renderOneDay(data)) // функция из файла Руслана OneDay
+    .catch(err => console.log(err));
+};
+getOneDayData();
 
-//  *********************** Делаем запрос на сервер и получаем данные
+// Делаем запрос на сервер и получаем данные за 5 дней
+// const getFiveDaysData = () => {
+//   return apiService
+//     .getData('forecast')
+//     .then(data => renderFiveDays(data))
+//     .catch(err => console.log(err));
+// };
 
-input.addEventListener('input', function (name) {
-  fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=b2e20a408d2fb97b7ae2d0be72bcb7e8',
-  )
-    .then(response => response.json())
-    .then(data => {
-      let tempValue = data['main']['temp'];
-      let nameValue = data['name'];
-      let descValue = data['weather'][0]['description'];
+// getFiveDaysData();
 
-      main.innerHTML = nameValue;
-      desc.innerHTML = 'Desc - ' + descValue;
-      temp.innerHTML = 'Temp - ' + tempValue;
-      input.value = '';
-    })
-    .catch(err => alert('Wrong city name!'));
-});
+const renderFiveDays = () => {
+  apiService.getData('forecast').then(data => {
+    const allDaysArr = data.list; // получаем массив из 40 объектов
+    console.log(allDaysArr);
+    const fiveDaysData = {};
+    data.list.forEach(item => {
+      const oneDay = new Date(item.dt_txt).getDate(); // это ключ массива
+      if (!fiveDaysData[oneDay]) {
+        fiveDaysData[oneDay] = [];
+      }
+      fiveDaysData[oneDay].push(item);
+      // console.log(oneDay);
+    });
+    console.log(fiveDaysData);
 
-const getWeatherData = data => {
-  fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=b2e20a408d2fb97b7ae2d0be72bcb7e8',
-  )
-    .then(response => response.json())
-    .then(data => {
-      let tempValue = data.main.temp;
-      console.log(tempValue);
-      let nameValue = data.name;
-      console.log(nameValue);
-      let descValue = data.weather[0].description;
-      console.log(descValue);
+    // console.log(dataDt);
 
-      // main.insertAdjacentElement('beforeend', nameValue);
-      // data.desc.innerHTML = 'Desc - ' + descValue;
-      // data.temp.innerHTML = 'Temp - ' + tempValue;
-      // input.value = '';
-      // console.log(data);
-    })
-    .catch(err => alert('Wrong city name!'));
+    // const oneDay = new Date(data.list.dt * 1000).getDate();
+    // const getDate = data => new Date(data.list.dt * 1000).getDate();
+    // getDate();
+
+    // const fiveDaysData = [];
+    // for (let i = 0; i < dataDt.length; i = i + 8) {
+    //   fiveDaysData.push(dataDt[i]);
+    // }
+
+    // const fiveDaysArr = dataDt.filter((elm, index, arr) => arr.indexOf(elm) === index);
+
+    // const fiveDaysArr = allDaysArr.filter(obj => fiveDaysData.includes(obj));
+    // console.log(fiveDaysArr);
+  });
 };
 
-//  *********************** Функции для получения данных с api
-// На один день:
+renderFiveDays();
 
-const getOneDayData = searchName => {
-  console.log(searchName);
-  location = searchName;
-  req = apiService.getData('weather');
-  console.log(req);
-  return getWeatherData(req).then(data => dataProcessingOneDay(data));
-};
-// getOneDayData();
-const getFiveDayData = () => {
-  req = apiService.getData('forecast');
-  return getWeatherData(req).then(response => dataProcessingFiveDays(response.data));
-}; // на 5 дней
-//  *********************** Получаем день недели
-const weekDayNow = data => {
-  const date = new Date(data * 1000);
-  const weekDay = new Intl.DateTimeFormat('en', { weekday: 'long' }).format(date);
-  return weekDay;
-};
-// *********************** Получаем месяц
-const monthNow = data => {
-  const date = new Date(data * 1000);
-  const month = new Intl.DateTimeFormat('en', { month: 'short' }).format(date);
-  return month;
-};
-// Получаем обьект icon data
-const getIconData = data => {
-  const date = new Date(data[0].dt * 1000);
-  date.setMilliseconds(0);
-  date.setSeconds(0);
-  date.setMinutes(0);
-  date.setHours(12);
-  const getTimeObj = data.find(e => e.dt == date.getTime() / 1000);
-  const iconInfo = {};
-  if (getTimeObj) {
-    const weather = getTimeObj.weather[0];
-    const icon = 'http://openweathermap.org/img/wn/' + weather.icon + '.png';
-    iconInfo.icon = icon;
-    iconInfo.iconDescription = weather.description;
-    return iconInfo;
-  } else {
-    let weather = {};
-    if (data[3]) {
-      weather = data[3].weather[0];
-    } else {
-      weather = data[0].weather[0];
-    }
-    const icon = 'http://openweathermap.org/img/wn/' + weather.icon + '.png';
-    iconInfo.icon = icon;
-    iconInfo.iconDescription = weather.description;
-    return iconInfo;
-  }
-};
+// Делаем запрос на сервер и получаем данные для more info
+// const getMoreInfoData = () => {
+//   return apiService
+//     .getData('forecast')
+//     .then(data => renderFiveDays(data))
+//     .catch(err => console.log(err));
+// };
 
-// Получить текущее время
-const getCurrentTime = data => {
-  const dataTime = new Date(data * 1000);
+// getMoreInfoData();
 
-  return addZero(dataTime.getHours()) + ':' + addZero(dataTime.getMinutes());
-};
-// Обработка данных на один день
-const dataProcessingOneDay = response => {
-  // const main = response.main;
-  // const sys = response.sys;
-  // const weather = response.weather[0];
-  oneDayData.city = response.name;
-  oneDayData.countryCode = response.sys.country;
-  oneDayData.temp = conToCel(main.temp);
-  oneDayData.tempMin = conToCel(main.temp_min);
-  oneDayData.tempMax = conToCel(main.temp_max);
-  oneDayData.sunrise = new Date(sys.sunrise * 1000);
-  oneDayData.sunset = new Date(sys.sunset * 1000);
-  oneDayData.icon = 'http://openweathermap.org/img/wn/' + weather.icon + '.png';
-  oneDayData.iconDescription = weather.description;
-  oneDayData.timezone = response.timezone;
-  return oneDayData;
-};
-// Обработка данных на 5 дней
-const getDate = data => new Date(data.dt * 1000).getDate();
-const dataProcessingFiveDays = response => {
-  const dates = response.list
-    .map(element => getDate(element))
-    .filter((el, idx, arr) => arr.indexOf(el) === idx);
-  const list = dates
-    .map(el => response.list.filter(elem => getDate(elem) === el))
-    .map(element => ({
-      DayNum: getDate(element[0]),
-      Day: weekDayNow(element[0].dt),
-      Month: monthNow(element[0].dt),
-      date: element[0].dt,
-      icon: getIconData(element),
-      forecast: element,
-      temp: mathTemp(element),
-    }));
-  if (list[5]) {
-    list.shift();
-  }
-  const changedData = {
-    ...response,
-    list,
-  };
-  fiveDayData = changedData;
-  return fiveDayData;
-};
-// Обработка данных для блока more info
-const dataProcessingMoreInfo = () => {
-  moreInfoData = fiveDayData.list.map(e => ({
-    date: e.date,
-    DayNum: e.DayNum,
-    forecast: e.forecast.map(e => ({
-      time: getCurrentTime(e.dt),
-      temp: Math.floor(e.main.temp - 273.15),
-      humidity: e.main.humidity,
-      pressure: e.main.pressure,
-      speed: Number(e.wind.speed.toFixed(1)),
-      icon: 'http://openweathermap.org/img/wn/' + e.weather[0].icon + '.png',
-      iconDescription: e.weather[0].description,
-    })),
-  }));
-  return moreInfoData;
-};
+// Делаем запрос на сервер и получаем данные для chart
+// const getChartData = () => {
+//   return apiService
+//     .getData('forecast')
+//     .then(data => renderFiveDays(data))
+//     .catch(err => console.log(err));
+// };
+
+// getChartData();
--- src/js/moreInfo.js
+++ src/js/moreInfo.js
@@ -1,28 +1,38 @@
 import moreInfoTpl from '../templates/moreInfo.hbs';
 import apiService from './apiService.js';
-import config from '../config.json';
+import helper from './base/helper.js';
 
-import refs from '../refs';
+// import refs from '../refs';
 
-// let moreInfoData = {};
+let moreInfoData = {};
 
-// const renderMoreInfo = target => {
-//   // moreInfoData = api.getData();
-//   document.querySelector('.additional-info').classList.remove('isHiden');
-//   // const day = Number(target.dataset.day);
-//   const moreDaysListItem = document.querySelectorAll('.more-info');
-//   if (moreDaysListItem) {
-//     moreDaysListItem.forEach(e => e.remove());
-//   }
-//   const currentMoreInfo = moreInfoData.find(e => e.DayNum == day);
-//   refs.moreInfoBlock.innerHTML += moreInfoTemp(currentMoreInfo.forecast);
-// };
+const getMoreInfoData = () => {
+  return apiService
+    .getData('forecast')
+    .then(data => renderFiveDays(data))
+    .catch(err => console.log(err));
+};
 
-// document.querySelector('.fivedays-weather').addEventListener('click', handleBtnlick);
+getMoreInfoData();
 
-// function handleBtnlick(event) {
-//   event.preventDefault();
-//   if (event.target.nodeName === 'BUTTON') {
-//     renderMoreInfo(target);
-//   }
-// }
+const renderMoreInfo = target => {
+  // moreInfoData = apiService.getData('forecast');
+  // console.log(moreInfoData);
+  document.querySelector('.hours-wether').classList.remove('.visually-hidden');
+  // const day = Number(target.dataset.day);
+  const moreDaysListItem = document.querySelectorAll('.more-info');
+  if (moreDaysListItem) {
+    moreDaysListItem.forEach(e => e.remove());
+  }
+  const currentMoreInfo = moreInfoData.find(e => e.DayNum == day);
+  refs.moreInfoBlock.innerHTML += moreInfoTemp(currentMoreInfo.forecast);
+};
+
+document.querySelector('.more-info-weather').addEventListener('click', handleBtnlick);
+
+function handleBtnlick(event) {
+  event.preventDefault();
+  if (event.target.nodeName === 'BUTTON') {
+    renderMoreInfo(target);
+  }
+}
