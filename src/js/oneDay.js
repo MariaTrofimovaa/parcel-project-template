4b9deed47d29c0225a06d201d903c808309888b6
Russel <blokhin.rus@gmail.com>
2021-05-28 10:32:13 +0300

    fivedays

--- src/js/oneDay.js
+++ src/js/oneDay.js
@@ -1,20 +1,30 @@
 import oneDayTpl from '../templates/oneDay.hbs';
 import apiService from '../js/apiService.js';
 
-// document.querySelector('.one-day').innerHTML = oneDayTpl();
-
 apiService.getData('weather').then(data => {
-  // data = data.map(e => Math.floor(e.main.temp - 273.15));
-  // const temp = {
-  //   TempMin: Math.min(...data),
-  //   TempMax: Math.max(...data),
-  // };
-
   const tplDate = {
-    ...data,
-    // temp: Math.round(...data),
+    name: data.name,
+    temp: Math.round(data.main.temp),
+    country: data.sys.country,
+    temp_min: Math.round(data.main.temp_min),
+    temp_max: Math.round(data.main.temp_max),
     icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
   };
   document.querySelector('.one-day').innerHTML = oneDayTpl(tplDate);
   console.log(data);
 });
+
+// const renderOneDay = data => {
+//   const tplDate = {
+//     name: data.name,
+//     temp: Math.round(data.main.temp),
+//     country: data.sys.country,
+//     temp_min: Math.round(data.main.temp_min),
+//     temp_max: Math.round(data.main.temp_max),
+//     icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+//   };
+//   document.querySelector('.one-day').innerHTML = oneDayTpl(tplDate);
+//   console.log(data);
+// };
+
+// export default renderOneDay;
