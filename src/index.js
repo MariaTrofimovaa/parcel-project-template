cd84e9c0922741cf6631f12d02ea7f3970f224da
Russel <blokhin.rus@gmail.com>
2021-05-31 13:26:00 +0300

    add

--- src/index.js
+++ src/index.js
@@ -2,19 +2,17 @@ import './sass/main.scss';
 
 import './js/apiService.js';
 
-
 import './js/base/helper.js';
 // import './js/components/buttons-service.js';
 import './js/components/bg-service.js';
 
-
 import './js/header.js';
-import './js/buttons.js';
 
 import './js/calendar.js';
 import './js/chart.js';
 import './js/fiveDays.js';
 import './js/moreInfo.js';
 import './js/oneDay.js';
+import './js/buttons.js';
 
 // import './js/base/refs.js';
