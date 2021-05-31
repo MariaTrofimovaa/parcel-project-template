1c4286c445c7102f2d8ba739d48452f9d7c90b68
Sergey Yavorskiy <yavoser@gmail.com>
2021-05-31 13:30:27 +0300

    change fav-list style

--- src/js/header.js
+++ src/js/header.js
@@ -84,6 +84,7 @@ const saveLocalStorage = () => {
 updateView();
 
 favoriteBtnRef.addEventListener('click', saveLocalStorage);
+
 // searchbox.addEventListener('submit', saveLocalStorage);
 
 // * Localstorage
